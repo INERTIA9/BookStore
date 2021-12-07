2c051eb3610eb900e4c13f4863ef9a25d2004aa1
saurabh <dekapurwars@gmail.com>
2021-12-07 21:11:15 +0530

    Added:Required Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginSignupComponent } from './Components/login-signup/login-signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HttpClientModule } from '@angular/common/http';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { HomepageComponent } from './Components/homepage/homepage.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatDividerModule} from '@angular/material/divider';
+import { GetallbooksComponent } from './Components/getallbooks/getallbooks.component';
+import { DisplaybooksComponent } from './Components/displaybooks/displaybooks.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginSignupComponent,
+    ForgotPasswordComponent,
+    HomepageComponent,
+    GetallbooksComponent,
+    DisplaybooksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatTabsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
