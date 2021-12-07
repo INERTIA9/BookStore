1ede16c17e08fea4869d8ef7ddafea445f3548e4
saurabh <dekapurwars@gmail.com>
2021-12-07 21:10:29 +0530

    Added:Child Path Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
+import { GetallbooksComponent } from './Components/getallbooks/getallbooks.component';
+import { HomepageComponent } from './Components/homepage/homepage.component';
+import { LoginSignupComponent } from './Components/login-signup/login-signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login-signup', pathMatch: 'full' },
+  { path: 'login-signup', component: LoginSignupComponent },
+  { path: 'forgotpassword', component: ForgotPasswordComponent },
+  {
+    path: 'homepage', component: HomepageComponent,
+    children: [
+      { path: 'getallbooks', component: GetallbooksComponent }
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
