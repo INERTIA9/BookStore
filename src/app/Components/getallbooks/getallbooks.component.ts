54e46a54e0d1a19f2b2c872be0a75942eca48371
saurabh <dekapurwars@gmail.com>
2021-12-09 07:50:50 +0530

    Added:Get all book Function

--- src/app/Components/getallbooks/getallbooks.component.ts
+++ src/app/Components/getallbooks/getallbooks.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from 'src/app/Services/bookService/book.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-getallbooks',
   templateUrl: './getallbooks.component.html',
@@ -7,18 +8,29 @@ import { BookService } from 'src/app/Services/bookService/book.service';
 })
 export class GetallbooksComponent implements OnInit {
   BookList: any
-  
-  constructor(private bookService: BookService) { }
+  BookCount: any
+  book:any
+  constructor(private bookService: BookService,private router: Router) { }
 
   ngOnInit(): void {
     this.getAllBooks();
   }
 
   getAllBooks() {
-    this.bookService.getallBookService().subscribe((result) => {
-      console.log(result);
-     this.BookList=result
-      
+    this.bookService.getallBookService().subscribe((response: any) => {
+      console.log(response);
+      this.BookList = response.result;
+      this.BookCount = response.result.length;
+      console.log(this.BookCount);
+
+
     })
   }
+  onquickview(book:any){
+    localStorage.setItem('bookId',book._id);
+    console.log("id",book._id);
+    this.router.navigateByUrl('/quickview')
+  
+  }
+ 
 }
