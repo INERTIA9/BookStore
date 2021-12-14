26e6bfff88a891f5e9a40ef6d342b5fb5fbead69
saurabh <dekapurwars@gmail.com>
2021-12-14 20:08:29 +0530

    Added: Added DeleteCart service

--- src/app/Services/bookService/book.service.ts
+++ src/app/Services/bookService/book.service.ts
@@ -13,17 +13,125 @@ export class BookService {
   }
 
 
- getallBookService() {
+  getallBookService() {
+    let headers = {
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'x-access-token': this.token,
+      })
 
-    
+    }
+    return this.httpService.getService('get/book', true, headers)
+  }
+
+  addtoCartService(productID: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'x-access-token': this.token,
+      })
+    }
+    return this.httpService.postService('add_cart_item/' + productID, null, true, headers)
+  }
+
+  updateCountService(productID: any, reqPayload: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'x-access-token': this.token,
+        'content-Type': 'application/json'
+      })
+    }
+    return this.httpService.putService('cart_item_quantity/' + productID, reqPayload, true, headers);
+  }
+
+  getCartItemsService() {
 
     let headers = {
       headers: new HttpHeaders({
         'Content-type': 'application/json',
-        'Authorization': this.token
+        'x-access-token': this.token,
       })
 
     }
-    return this.httpService.getService('get/book',true, headers)
+    return this.httpService.getService('get_cart_items', true, headers)
+  }
+
+  addtoWishlistService(productID: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'x-access-token': this.token,
+      })
+    }
+    return this.httpService.postService('add_wish_list/' + productID, null, true, headers)
+  }
+
+  getWishlistService() {
+    let headers = {
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'x-access-token': this.token,
+      })
+
+    }
+    return this.httpService.getService('get_wishlist_items', true, headers)
+  }
+
+
+  deleteWishlistService(productID: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'x-access-token': this.token,
+      })
+    }
+    return this.httpService.deleteService('remove_wishlist_item/' + productID, null, true, headers)
+  }
+
+  checkOut(reqData: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'x-access-token': this.token,
+      })
+    }
+    return this.httpService.postService('add/order', reqData, true, headers)
+  }
+
+  removeCartitemService(productID: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'x-access-token': this.token,
+      })
+    }
+    return this.httpService.deleteService('remove_cart_item/' + productID, null, true, headers)
+  }
+  addfeedbackService(productID: any, reqPayload: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'x-access-token': this.token,
+        'content-Type': 'application/json'
+      })
+    }
+    return this.httpService.postService('add/feedback/' + productID, reqPayload, true, headers);
+  }
+
+  getfeedBack(data: any) {
+    this.token = localStorage.getItem('token')
+    let options = {
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'x-access-token': this.token
+      })
+    }
+   
+    return this.httpService.getService(`get/feedback/${data.product_id}`, true, options);
+
   }
 }
+
+
+
+
+
