d3cd2a2e24024f7b4ab2843bde6c10ce8cfa2afe
saurabh <dekapurwars@gmail.com>
2021-12-14 20:02:35 +0530

    Added:Declared Required Variables

--- src/app/Components/getallbooks/getallbooks.component.ts
+++ src/app/Components/getallbooks/getallbooks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from 'src/app/Services/bookService/book.service';
 import { Router } from '@angular/router';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-getallbooks',
   templateUrl: './getallbooks.component.html',
@@ -9,28 +11,89 @@ import { Router } from '@angular/router';
 export class GetallbooksComponent implements OnInit {
   BookList: any
   BookCount: any
-  book:any
-  constructor(private bookService: BookService,private router: Router) { }
+  book: any
+  selected = 'option2';
+  toggle = true;
+  status = 'Enable';
+  cartList: any;
+  cartlistcount: any;
+  page: number = 1;
+  totallength:any
+
+
+
+
+
+  constructor(private bookService: BookService, private router: Router,private _snackBar: MatSnackBar) {
+
+  }
 
   ngOnInit(): void {
     this.getAllBooks();
+    
+
   }
 
   getAllBooks() {
+   
     this.bookService.getallBookService().subscribe((response: any) => {
       console.log(response);
       this.BookList = response.result;
+     this.totallength=response.result.length
       this.BookCount = response.result.length;
+      console.log("BookList======>",this.BookList);
       console.log(this.BookCount);
 
+    },error=>{console.log(error);
+    })
+  }
+ 
+
+  onquickview(book: any) {
+    localStorage.setItem('bookId', book._id);
+    console.log("id", book._id);
+    this.router.navigateByUrl('/homepage/quickview/' + book._id)
+
+  }
+
+  sortlowtohigh() {
+    this.BookList.sort((a: any, b: any) => a.price - (b.price));
+  }
+  sorthightolow() {
+    this.BookList.sort((a: any, b: any) => b.price - (a.price));
+  }
+
+  newestarrivalse() {
+    this.BookList.reverse()
+  }
+
+
+  enableDisableRule() {
+    this.toggle = !this.toggle;
+    this.status = this.toggle ? 'Enable' : 'Disable';
+  }
+
+  addtowishlist(book:any) {
+    this.bookService.addtoWishlistService(book._id).subscribe((response:any)=>{
+      console.log(response);
+
+      this._snackBar.open("Item Removed From Wishlist", '', {
+        duration: 2000,
+        panelClass: ['brown-snackbar']
+      })
+     
+      
+    })
+
+  }
+
+  addtocart(book:any){
+    this.bookService.addtoCartService(book._id).subscribe((response: any) => {
+      console.log(response);
+
+    }, error => {
+      console.log(error);
 
     })
   }
-  onquickview(book:any){
-    localStorage.setItem('bookId',book._id);
-    console.log("id",book._id);
-    this.router.navigateByUrl('/quickview')
-  
-  }
- 
 }
