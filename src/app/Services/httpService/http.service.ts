9f8cbff59df2b23b6ad2d47a4bf78a7b45757067
saurabh <dekapurwars@gmail.com>
2021-12-14 20:07:54 +0530

    Added:Added put and Delete Service

--- src/app/Services/httpService/http.service.ts
+++ src/app/Services/httpService/http.service.ts
@@ -9,15 +9,23 @@ export class HttpService {
   BaseUrl = environment.BaseUrl
   constructor(private httpClient: HttpClient) { }
 
-  postService(url: string = '', reqPayload: Object = {}, token: boolean = false, httpOptions: any = {}) {
+  postService(url: string = '', reqPayload: any = null, token: boolean = false, httpOptions: any = {}) {
     console.log(reqPayload);
     return this.httpClient.post(this.BaseUrl + url, reqPayload, token && httpOptions)
 
   }
   
   getService(url: string = '', token: boolean = false, httpOptions: any = null) {
-
     return this.httpClient.get(this.BaseUrl + url, token && httpOptions)
   }
-}
 
+  putService(url: string = '', reqPayload: any = null, token: boolean = false, httpOptions: any = {}) {
+    console.log(reqPayload);
+    return this.httpClient.put(this.BaseUrl + url, reqPayload, token && httpOptions)
+
+  }
+  deleteService(url: string = '', reqPayload: any = null, token: boolean = false, httpOptions: any = null) {
+    return this.httpClient.delete(this.BaseUrl + url, token && httpOptions)
+  }
+
+}
