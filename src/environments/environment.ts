8d6c89a093e39799e2142a5d1c6dcb0fdc79fd69
saurabh <dekapurwars@gmail.com>
2021-12-14 20:07:03 +0530

    Added:Modified Base Url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  BaseUrl:'https://new-bookstore-backend.herokuapp.com/bookstore_user/'
+  BaseUrl:'https://bookstore.incubation.bridgelabz.com/bookstore_user/'
 };
 
 /*
