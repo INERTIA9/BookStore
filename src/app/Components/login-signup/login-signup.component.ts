00c809725436019896d00d93b84192fac9f4f30d
saurabh <dekapurwars@gmail.com>
2021-12-18 20:51:50 +0530

    Added:Mat-select value validator

--- src/app/Components/login-signup/login-signup.component.ts
+++ src/app/Components/login-signup/login-signup.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/Services/userService/user.service';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+
+
 @Component({
   selector: 'app-login-signup',
   templateUrl: './login-signup.component.html',
@@ -19,42 +21,42 @@ export class LoginSignupComponent implements OnInit {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email, Validators.pattern('[[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$]*')]],
       password: ['', [Validators.required, Validators.minLength(6)]],
+      selectedValue: ['', [Validators.required]],
     });
 
     this.signupForm = this.formBuilder.group({
       fullName: ['', [Validators.required, Validators.pattern(/(?=^.{0,40}$)^[a-zA-Z-]+\s[a-zA-Z-]+$/g)]],
       email: ['', [Validators.required, Validators.email, Validators.pattern('[[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$]*')]],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      mobileNumber: ['', [Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+      mobileNumber: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+      selectedValue: ['', [Validators.required]],
       service: ['advance']
     });
   }
 
 
- 
-  onLogin() {
 
+  onLogin() {
+    let reqData = {
+
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    }
     console.log(this.loginForm.value);
     if (this.loginForm.valid) {
-      let reqData = {
-        service: this.loginForm.value.service,
-        email: this.loginForm.value.email,
-        password: this.loginForm.value.password
-      }
-
+      if(this.loginForm.value.selectedValue=="User"){
       console.log("valid");
+      console.log("User");
       this.userservice.loginUserService(reqData).subscribe((result: any) => {
-        
-      
         console.log(result);
-        localStorage.setItem('token',result.result.accessToken);
+        localStorage.setItem('token', result.result.accessToken);
         this._snackBar.open("Login Successfull", '', {
           duration: 2000,
           panelClass: ['brown-snackbar']
         })
-
         this.router.navigateByUrl('/homepage/getallbooks')
-       
+      
+  
       }, error => {
         console.log(error);
         this._snackBar.open("Enter Valid Data", '', {
@@ -63,34 +65,50 @@ export class LoginSignupComponent implements OnInit {
         })
 
       })
+    }else if(this.loginForm.value.selectedValue=="Admin"){
+      this.userservice.adminLoginService(reqData).subscribe((result:any)=>{
+        console.log(result);
+        localStorage.setItem('token',result.result.accessToken)
+      })
+      this.router.navigateByUrl('/homepage/admin')
+      this._snackBar.open("Welcome To Admin Portal", '', {
+        duration: 2000,
+        panelClass: ['brown-snackbar']
+      })
+
     }
 
   }
 
-
+  }
 
   onSignup() {
     this.submitted = true
+    let reqData = {
+      service: this.signupForm.value.service,
+      fullName: this.signupForm.value.fullName,
+      email: this.signupForm.value.email,
+      password: this.signupForm.value.password,
+      phone: this.signupForm.value.mobileNumber
+    }
     console.log(this.signupForm.value);
     if (this.signupForm.valid) {
-      let reqData = {
-        service: this.signupForm.value.service,
-        fullName: this.signupForm.value.fullName,
-        email: this.signupForm.value.email,
-        password: this.signupForm.value.password,
-        phone: this.signupForm.value.mobileNumber
+      if (this.signupForm.value.selectedValue == 'User') {
+        console.log("user");
+        console.log("valid");
+        this.userservice.registerUserService(reqData).subscribe((response: any) => {
+          console.log(response);
+        }, error => {
+          console.log(error);
+        });
       }
-      console.log("valid");
-      this.userservice.registerUserService(reqData).subscribe((response: any) => {
-        console.log(response);
-       
-       
+      else if (this.signupForm.value.selectedValue == 'Admin') {
+        console.log("admin");
+        this.userservice.adminregistrationService(reqData).subscribe((response) => {
+          console.log(response);
 
-      }, error => {
-        console.log(error);
-      });
-
-
+        })
+      }
     }
     else {
       console.log("invalid");
@@ -102,7 +120,5 @@ export class LoginSignupComponent implements OnInit {
 }
 
 
-function token(token: any) {
-  throw new Error('Function not implemented.');
-}
+
 
